57d6c7b3bd29faabb1fe2e5ece162d5e8dab50da
hwanseung2 <wazs555@hufs.ac.kr>
2022-05-18 21:36:35 +0900

    연관분석, Apriori, FP-Growth

---
+++ t_academy/01_apriori_fp_growth.py
@@ -0,0 +1,11 @@
+import numpy as np
+import mlxtend
+import pandas as pd
+
+data = np.array([
+    ['우유', '기저귀', '쥬스'],
+    ['양상추', '기저귀', '맥주'],
+    ['우유', '양상추', '기저귀', '맥주'],
+    ['양상추', '맥주']
+])
+
